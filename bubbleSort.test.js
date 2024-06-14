f0c1cca25ef9734f5d39cb5051900e97df965429
Cameron Juresich <camjuresich@gmail.com>
2024-06-13 17:16:47 -1000

    Updated bubbleSort to break out early when no swaps are present in the current iteration.

---
+++ bubbleSort.test.js
@@ -0,0 +1,13 @@
+const bubbleSort = require('./bubbleSort.js')
+
+test('should be able to sort an array', () => {
+    expect(bubbleSort([10,9,8,7,6,5,4])).toStrictEqual([4,5,6,7,8,9,10])
+})
+
+test('should be able to sort an array', () => {
+    expect(bubbleSort([0,0,1,1,2,22,22,22,22,3,45,69])).toStrictEqual([0,0,1,1,2,3,22,22,22,22,45,69])
+})
+
+test('should be able to sort an array with negative values', () => {
+    expect(bubbleSort([1,2,5,4,6,9,-1])).toStrictEqual([-1,1,2,4,5,6,9])
+})
