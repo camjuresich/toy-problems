39adb6eb1b97443aeba3c20db921de1e9410378f
Cameron Juresich <camjuresich@gmail.com>
2024-06-13 17:49:25 -1000

    commented out my console logs to clean up the test results.

--- bubbleSort.js
+++ bubbleSort.js
@@ -12,7 +12,7 @@ function bubbleSort(arr) {
     for (var i = arr.length; i > 0; i--) {
         noSwaps = true;
         for (var j = 0; j < i - 1; j++) {
-            console.log(arr, arr[j], arr[j + 1]);
+            // console.log(arr, arr[j], arr[j + 1]);
             if (arr[j] > arr[j + 1]) {
                 var temp = arr[j];
                 arr[j] = arr[j + 1];
